7b37ba88d81f244c0fbccaa459f48507d836f9e7
Ian Weber <iacweber@gmail.com>
2024-07-17 15:49:36 -0700

    credits

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,6 +2,10 @@ extends Control
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/Game.tscn")
+	
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://scenes/Credits.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
